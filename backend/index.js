a335217ded2c48cb620fccfc6ce8f5d95b335f2c
naieem-bd <naieemur@gmail.com>
2021-09-19 16:12:31 +0600

    users router added

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,21 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
+const userRoute = require('./routes/users')
 
 dotenv.config()
 
-mongoose.connect(process.env.DB_URL,
-  () => {
-    console.log('MongoDB Connected!')
-  }
-)
+mongoose.connect(process.env.DB_URL, () => {
+  console.log('MongoDB Connected!')
+})
+
+// middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use('/api/users', userRoute)
+
 
 app.listen(2727, () => {
   console.log('Server is running on port 2727')
