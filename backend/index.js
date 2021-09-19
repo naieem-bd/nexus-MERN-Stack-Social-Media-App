56aeeebdb8248f47d1afa44acdc98a6afbc693e2
naieem-bd <naieemur@gmail.com>
2021-09-19 16:46:54 +0600

    auth route added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(helmet())
 app.use(morgan("common"))
 
 app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
 
 
 app.listen(2727, () => {
