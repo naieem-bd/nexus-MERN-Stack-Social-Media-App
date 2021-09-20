e66f8e79f2acdc3fc3a5b60c7379f864d837a0ac
naieem-bd <naieemur@gmail.com>
2021-09-20 15:00:50 +0600

    auth route, bcrypt added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
-router.get('/', (req, res) => {
-  res.send('auth router')
+// register screen
+router.post('/register', async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  try {
+    const salt = await bcrypt.genSalt(10)
+    
+    const user = await newUser.save()
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
